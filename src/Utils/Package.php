e88de492e0b58c01e16dfbeec19646937f27caab
David Angulo <david.angulo.arias@gmail.com>
2020-05-21 13:57:54 -0500

    utility class for working with the package itself and compressing it

---
+++ src/Utils/Package.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace SugarPack\Utils;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use ZipArchive;
+
+class Package
+{
+    /**
+     * Path to the package folder containing the manifest
+     *
+     * @var string
+     */
+    private $path = null;
+
+    /**
+     * Files allowed by SugarCrm Module loader
+     *
+     * @var array
+     */
+    private $allowedFiles = [
+        'css', 'gif', 'hbs', 'htm', 'html', 'jpg', 'js', 'md5',
+        'pdf', 'php', 'png', 'tpl', 'txt', 'xml'
+    ];
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function isValid() : bool
+    {
+        if (!is_dir($this->path)) {
+            return false;
+        }
+
+        $files = scandir($this->path);
+
+        if (!in_array('manifest.php', $files)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function compress(string $output) : bool
+    {
+        $zip = new ZipArchive;
+
+        if (!$zip->open($output, ZipArchive::CREATE | ZipArchive::OVERWRITE)) {
+            return false;
+        }
+
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($this->path),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+
+
+        foreach ($files as $file) {
+            // Skip directories (they would be added automatically)
+            if ($file->isDir()) {
+                continue;
+            }
+
+            if (!in_array($file->getExtension(), $this->allowedFiles)) {
+                continue;
+            }
+
+            // Get real and relative path for current file
+            $filePath = $file->getRealPath();
+            $relativePath = $this->removeEverythingBefore($filePath, basename($this->path) . DIRECTORY_SEPARATOR);
+
+            // Add current file to archive
+            $zip->addFile($filePath, $relativePath);
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+
+        return true;
+    }
+
+    private function removeEverythingBefore($in, $before)
+    {
+        $pos = strpos($in, $before);
+        return $pos !== false
+        ? substr($in, $pos + strlen($before), strlen($in))
+        : "";
+    }
+}

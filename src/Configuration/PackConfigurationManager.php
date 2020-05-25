a1e3cb4694a130261226e562d3cd4a4634294460
David Angulo <david.angulo.arias@gmail.com>
2020-05-25 15:07:20 -0500

    Create config file if it doesnt exist

--- src/Configuration/PackConfigurationManager.php
+++ src/Configuration/PackConfigurationManager.php
@@ -6,11 +6,17 @@ use Symfony\Component\Yaml\Yaml;
 
 class PackConfigurationManager
 {
+    /**
+     * Gets the config from the config file or creates it and returns the default
+     *
+     * @return array
+     */
     public static function getConfigOrDefault(): array
     {
         try {
             return Yaml::parseFile(static::getPathToConfigFile());
         } catch (\Exception $e) {
+            self::createConfigFile();
             return ConfigurationGenerator::getDefaultPackConfig();
         }
     }
@@ -18,5 +24,15 @@ class PackConfigurationManager
     public static function getPathToConfigFile() : string
     {
         return OS::getConfigDir() . DIRECTORY_SEPARATOR . 'pack.yaml';
+    }
+
+    private static function createConfigFile(): void
+    {
+        mkdir(OS::getConfigDir(), 0777, true);
+
+        @file_put_contents(
+            static::getPathToConfigFile(),
+            ConfigurationGenerator::getDefaultPackYamlConfig()
+        );
     }
 }
